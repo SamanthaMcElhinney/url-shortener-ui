fe21a913a86cd0ff32073dde1263f1362b2f4941
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-12 08:49:24 -0700

    feat:add POST

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrls = (input) => {
+  return fetch("http://localhost:3001/api/v1/urls", {
+    method:'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({input})
+  })
+  .then(response => response.json())
+}

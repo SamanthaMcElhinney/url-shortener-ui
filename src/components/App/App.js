1783fc5d74c603849077c0a45fe54ac5a9354ab3
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-12 08:49:49 -0700

    feat: add method to POST and add new URL to DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { getUrls } from '../../apiCalls';
+import { getUrls, postUrls } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
@@ -23,12 +23,22 @@ export class App extends Component {
     })
   }
 
+  addLink = (inputs) => {
+    postUrls(inputs)
+    .then(data => {
+      this.setState({urls:[...this.state.urls, inputs]})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
   render() {
     return (
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm addLink = {this.addLink}/>
         </header>
 
         <UrlContainer urls={this.state.urls}/>
